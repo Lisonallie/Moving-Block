a7c2bafe83f2c8d31971e6963f0eb73df858b0cb
Lisonallie <allisonvanlinden@gmail.com>
2019-06-18 10:26:05 +0200

    add specifications of boy size

---
+++ myscript.js
@@ -0,0 +1,37 @@
+var snake= null;
+			function init(){
+				snake=document.getElementById("snake");				
+				snake.style.position='relative';
+				snake.style.left='0px';
+				snake.style.top='0px';
+			}
+			function getKeyAndMove(e){				
+				var key_code=e.which||e.keyCode;
+				switch(key_code){
+					case 37: //left arrow key
+						moveLeft();
+						break;
+					case 38: //Up arrow key
+						moveUp();
+						break;
+					case 39: //right arrow key
+						moveRight();
+						break;
+					case 40: //down arrow key
+						moveDown();
+						break;						
+				}
+			}
+			function moveLeft(){
+				snake.style.left=parseInt(snake.style.left)-7 +'px';
+			}
+			function moveUp(){
+				snake.style.top=parseInt(snake.style.top)-7 +'px';
+			}
+			function moveRight(){
+				snake.style.left=parseInt(snake.style.left)+7 +'px';
+			}
+			function moveDown(){
+				snake.style.top=parseInt(snake.style.top)+7 +'px';
+			}
+			window.onload=init;
