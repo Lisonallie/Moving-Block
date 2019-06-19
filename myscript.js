d5f60dbf7a21c2a2ce3d5683ab291a66910ccff6
Lisonallie <allisonvanlinden@gmail.com>
2019-06-19 09:39:43 +0200

    try to create collision function

--- myscript.js
+++ myscript.js
@@ -37,27 +37,65 @@ function getKeyAndMove(e) {
 
 function moveLeft() {
     snake.style.left = parseInt(snake.style.left) - 7 + 'px';
-    update();
+    collisionCheck(slither, mouse);
 }
 function moveUp() {
     snake.style.top = parseInt(snake.style.top) - 7 + 'px';
-    update();
+    collisionCheck(slither, mouse);
 }
 function moveRight() {
     snake.style.left = parseInt(snake.style.left) + 7 + 'px';
-    update();
+    collisionCheck(slither, mouse);
 }
 function moveDown() {
     snake.style.top = parseInt(snake.style.top) + 7 + 'px';
-    update();
+    collisionCheck(slither, mouse);
 }
 window.onload = init;
 
-var update = () => {
+// var update = () => {
+//     if (
+//         mouse.x === slither.x || mouse.y === slither.y || mouse.y === slither.y && mouse.x === slither.x
+//     ) {
+//         mouse.x = Math.floor((Math.random() * 30) + 1) + 'px';
+//         mouse.y = Math.floor((Math.random() * 30) + 1) + 'px';
+//     }
+// };
+
+function collisionCheck(snake, mouse) {
+    var snakeBounds = slither.getBoundingClientRect();
+    var mouseBounds = mouse.getBoundingClientRect();
+
+    var snakeCenter = {
+      x: snakeBounds.left + (snakeBounds.width / 2),
+      y: snakeBounds.top + (snakeBounds.height / 2)
+    }
+
+    var mouseCenter = {
+      x: mouseBounds.left + (mouseBounds.width / 2),
+      y: mouseBounds.top + (mouseBounds.height / 2)
+    }
+
+    // initialize if element 1 is within the viewport
     if (
-        mouse.x === slither.x || mouse.y === slither.y || mouse.y === slither.y && mouse.x === slither.x
+        snakeBounds.top >= 0 &&
+        snakeBounds.left >= 0 &&
+        snakeBounds.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        snakeBounds.right <= (window.innerWidth || document.documentElement.clientWidth)
     ) {
-        mouse.x = Math.floor((Math.random() * 30) + 1) + 'px';
-        mouse.y = Math.floor((Math.random() * 30) + 1) + 'px';
+
+      // see https://stackoverflow.com/a/17628488/2116041
+      var distance = Math.sqrt(
+        Math.pow(snakeBounds.x - mouseBounds.x, 2) + 
+        Math.pow(snakeBounds.y - mouseBounds.y, 2) 
+      );
+
+      if (distance > snakeBounds.width && distance > snakeBounds.height) {
+        // no collision
+        return false; 
+      } else {
+        // collision detected!
+        return true; 
+      }
     }
 };
